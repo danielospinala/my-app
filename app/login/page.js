146f6c218d2dce2a5d966aa85834f45b1107a49d
nielda <danielospina@gmail.com>
2023-05-19 16:19:47 -0600

    log in

---
+++ app/login/page.js
@@ -0,0 +1,11 @@
+export default function Login() {
+   
+    return (
+        <div>
+            <h1>Welcome</h1>
+            <p>This is the login page </p>
+            
+
+        </div>
+    )
+}

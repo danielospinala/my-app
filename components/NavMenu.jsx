b5b692127389b43d68485a8377c0843ef4c8fb7f
nielda <danielospina@gmail.com>
2023-05-23 14:12:07 -0600

    nav bar done

--- components/NavMenu.jsx
+++ components/NavMenu.jsx
@@ -3,13 +3,14 @@ import Link from "next/link"
 import Image from "next/image"
 import styles from './NavMenu.module.css';
 import { useState, useEffect } from "react";
+import { signIn, signOut, useSession, getProviders } from 'next-auth/react'
 
 
 function NavMenu() {
     const isUserLoggedIn = false;
 
     return (
-        <div className={styles.navbar}>
+        <nav className={styles.navbar}>
             <Link href={'/'}>
                 <Image
                     src="/Logo_TV_2015.svg"
@@ -19,53 +20,58 @@ function NavMenu() {
                 />
             </Link>
             <div>
-            { isUserLoggedIn ? 
-            (
-            
-            <div>
-                <Link href={'/'}>
-                <button> logout</button>
-                </Link>
-                
-                <Image
-                src="/Logo_TV_2015.svg"
-                width={30}
-                height={30}
-                alt="profile"
-                
-                />
+                {isUserLoggedIn ?
+                    (
+
+                        <div>
+                            <Link href={'/'}>
+                                <button> logout</button>
+                            </Link>
+
+                            <Image
+                                src="/Logo_TV_2015.svg"
+                                width={30}
+                                height={30}
+                                alt="profile"
+
+                            />
+                        </div>
+                    ) :
+                    (<>
+                        <ul className={styles.list}>
+
+                            <li>
+                                <Link href={'/about'}>About</Link>
+                            </li>
+                            <li>
+                                <Link href={'/contact'}>Contact</Link>
+
+                            </li>
+                            <li>
+
+                            </li>
+                            <li>
+                                <Link href={'/login'}>
+                                    <button> Login</button>
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href={'/login'}>
+                                    <button> SignUp</button>
+                                </Link>
+                            </li>
+
+                        </ul>
+                    </>)}
+
             </div>
-             ): 
-            (<>
-                <ul className={styles.list}>
-                
-                <li>
-                    <Link href={'/about'}>About</Link>
-                </li>
-                <li>
-                    <Link href={'/contact'}>Contact</Link>
-
-                </li>
-                <li>
-                    
-                </li>
-                <li>
-                <Link href={'/login'}>
-                <button> login</button>
-                </Link>
-                </li>
-
-            </ul>
-            </>)}
-
-            </div>
-            
-            
 
 
 
 
-        </div>
+
+
+        </nav>
     )
 }
 
